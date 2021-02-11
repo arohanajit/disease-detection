7ef4bbbc2175e33d1ad9757bfaf49b5604426151
Arohan Ajit <arohanajit232@gmail.com>
2021-02-11 17:27:05 +0530

    Update loader.py

--- loader.py
+++ loader.py
@@ -1,7 +1,7 @@
 import os
 
 def load_data():
-    os.system('wget https://raw.githubusercontent.com/arohanajit/disease-detector/master/kaggle.json --no-check-certificate')
+    os.system('wget https://raw.githubusercontent.com/arohanajit/disease-detection/main/kaggle.json --no-check-certificate')
     os.system('pip install -q kaggle')
     os.system('mkdir -p ~/.kaggle')
     os.system('cp kaggle.json ~/.kaggle/')
